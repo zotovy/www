4de15131e7ed8ad5afce5ce844fd7366f50b946d
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-05-01 15:02:41 +0500

    improve seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,29 @@
-import React from "react";
+import React from "react"
 import { Helmet } from "react-helmet"
 import styled from "styled-components"
-import Section1 from "@/sections/section-1";
-import Section2 from "@/sections/section-2";
-import Section3 from "@/sections/section-3";
-import Section4 from "@/sections/section-4";
-import Section5 from "@/sections/section-5";
-import Section6 from "@/sections/section-6";
+import Section1 from "@/sections/section-1"
+import Section2 from "@/sections/section-2"
+import Section3 from "@/sections/section-3"
+import Section4 from "@/sections/section-4"
+import Section5 from "@/sections/section-5"
+import Section6 from "@/sections/section-6"
+import Seo from "../seo";
 
 const Container = styled.div`
 
-`;
+`
 
 export default function IndexPage() {
   return <Container>
     <Helmet>
       <title>Yaroslav Zotov | Fullstack developer</title>
+      <Seo/>
     </Helmet>
-    <Section1/>
-    <Section2/>
-    <Section3/>
-    <Section4/>
-    <Section5/>
-    <Section6/>
+    <Section1 />
+    <Section2 />
+    <Section3 />
+    <Section4 />
+    <Section5 />
+    <Section6 />
   </Container>
 }
---
+++ src/seo.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Seo() {
+  return <React.Fragment>
+    <meta name="title" content="Yaroslav Zotov | Fullstack developer" />
+    <meta name="description" content="Personal portfolio of Fullstack web and mobile developer Yaroslav Zotov" />
+    <meta name="keywords"
+          content="react, nextjs, gatsby, development, developer, fullstack, flutter, database, programmer, code, coding, mongodb, postgres, typescript, ui, ux. express, expressjs, asp, dotnet, api, website, mobile, app, webapp, pwa, spa, ssr, sass, css, scss, webdeveloper, webdev" />
+    <meta name="robots" content="index, follow" />
+    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="English" />
+  </React.Fragment>
+}
