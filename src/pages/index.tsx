8573ac32431e48cb4cb859ae96c52ed07ebdeb3b
imfunnyducj <107118136+imfunnyduck@users.noreply.github.com>
2022-07-03 15:53:46 +0500

    delete old portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
 import React from "react"
-import Main from "@/sections/main"
-import TechStack from "@/sections/tech-stack"
-import AboutMe from "@/sections/about-me"
-import MyProjects from "@/sections/my-projects"
-import ContactMe from "@/sections/contact-me"
-import Seo from "../seo";
+import Seo from "seo";
 
 export default function IndexPage() {
   return <div>
     <Seo/>
-    <Main />
-    <TechStack />
-    <AboutMe />
-    <MyProjects />
-    <ContactMe />
+
   </div>
 }
