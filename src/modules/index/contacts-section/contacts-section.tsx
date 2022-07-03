c51fa7e49de2f8638943c91d1d7436ba79bfbdde
imfunnyducj <107118136+imfunnyduck@users.noreply.github.com>
2022-07-03 23:37:49 +0500

    feat(landing): improve adaptive

--- src/modules/index/contacts-section/contacts-section.tsx
+++ src/modules/index/contacts-section/contacts-section.tsx
@@ -8,10 +8,13 @@ export const ContactsSection: React.FC = () => {
         <div className="contacts-content">
             <div className="about">
                 <div className="title">I’m building fullstack apps any size</div>
-                <p>Focused on digital design over these years I helped companies and growing startup build apps and
-                    websites of all sorts.</p>
+                <p>
+                    Focused on digital design over these years I helped companies and growing startup build apps and
+                    websites of all sorts.
+                </p>
                 <div className="line"/>
-                <a href="/about">About &amp; Services
+                <a href="/about">
+                    About &amp; Services
                     <svg width="20" height="11" xmlns="http://www.w3.org/2000/svg">
                         <path d="M14.285.668l-.506.506 4.146 4.148H.392v.717h17.533l-4.146 4.147.506.506 5.012-5.012z"
                               fill="#18033C" fill-rule="evenodd"/>
