26625d13bea1e9859d6b4edd6c87b6e7fae5cb85
zotovy <the1ime@yandex.ru>
2024-03-10 20:54:05 +0300

    feat: start v3

--- wrapRootElement.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ParallaxProvider } from 'react-scroll-parallax';
-
-export default ({ element }: any) => {
-  return <ParallaxProvider>
-    { element }
-  </ParallaxProvider>
-}
