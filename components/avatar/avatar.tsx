0a6638fb9dfe55fbb9b130461a353a845a1fa06a
zotovy <the1ime@yandex.ru>
2024-03-26 17:31:28 +0500

    feat(home): replace avatar img with next Image

--- components/avatar/avatar.tsx
+++ components/avatar/avatar.tsx
@@ -1,10 +1,9 @@
 import { FC, memo } from "react";
 import styles from "./avatar.module.scss"
+import Image from "next/image";
 
-export type AvatarProps = {
-
-}
+export type AvatarProps = {}
 
 export const Avatar: FC<AvatarProps> = memo(() => {
-    return <img className={styles.avatar} src="/images/avatar.png" alt=""/>
+    return <Image className={ styles.avatar } src="/images/avatar.png" alt="" width={ 64 } height={ 64 }/>
 })
